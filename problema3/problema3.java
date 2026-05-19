d145905cda705227c0de97828520e2ee25654345
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 22:03:05 -0500

    estructura de el problema tres

---
+++ problema3/problema3.java
@@ -0,0 +1,15 @@
+public class problema3 {
+
+    public static int inversiones(int[] array) {
+
+    }
+
+   public static void main(String[] args) {
+
+        int[] array = {3, 1, 2};
+
+        int cantidad = inversiones(array);
+
+        System.out.println("cantidad de inversiones: " + cantidad);
+    }
+}
