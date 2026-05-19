0642b4a55222e55dae64fd5791941e4369eaaf04
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 21:34:08 -0500

    agrego caso base para encontrar mayor

--- problema2/problema2.java
+++ problema2/problema2.java
@@ -1,5 +1,27 @@
 public class problema2 {
 
+    public static int[] mayores(int[] array, int i, int f) {
+
+        if (i == f) {
+
+            int[] auxiliar = new int[2];
+
+            auxiliar[0] = array[i];
+            auxiliar[1] = -1;
+
+            return auxiliar;
+        }
+
+        int medio = (i + f) / 2;
+
+        int[] izq = mayores(array, i, medio);
+        int[] drc = mayores(array, medio + 1, f);
+
+        int[] resultado = new int[2];
+
+        return resultado;
+    }
+
     public static void main(String[] args) {
 
         int[] array = {8, 3, 10, 5, 7};
