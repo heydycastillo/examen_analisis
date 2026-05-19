b84b98733c5ef0828ebdd738b6589b394a46f976
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 21:03:17 -0500

    creo la parte inicial del algoritmo con los datos y llamando a las funciones

---
+++ problema1/arreglo_ordenado.java
@@ -0,0 +1,17 @@
+public class arreglo_ordenado {
+    
+    public static boolean identificador(int[] array, int i,int n) {
+
+    }
+    
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 5, 8};
+        int n =4;
+        int[] array2 = {1, 4,2,8};
+        int x =3;
+        System.out.println("arreglo 1:");
+        System.out.println(identificador(array, 0,n));
+        System.out.println("arreglo2:");
+        System.out.println(identificador(array2, 0,x));
+    }
+}
