4b28552a23ad99723a8e41123099777caf3a9f1d
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 22:09:54 -0500

    agrega estrucrura del merge sort

--- problema3/problema3.java
+++ problema3/problema3.java
@@ -1,7 +1,20 @@
 public class problema3 {
 
     public static int inversiones(int[] array) {
+        int[] temporal = new int[array.length];
+        return mergesort(array, temporal, 0, array.length - 1);
+    }
+    public  static int mergesort(int[] array, int[] temporal, int izquierda, int derecha) {
+        int contador = 0;
 
+        if (izquierda < derecha) {
+            int medio = (izquierda + derecha) / 2;
+
+            contador += mergesort(array, temporal, izquierda, medio);
+            contador += mergesort(array, temporal, medio + 1, derecha);
+        }
+
+        return contador;
     }
 
    public static void main(String[] args) {
