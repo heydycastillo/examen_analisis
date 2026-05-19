d2e0cd4cb8d24b8b73bb093b3d476a562a772885
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 21:40:36 -0500

    se usa la recursividad para encontrar al segundo mayor

--- problema2/problema2.java
+++ problema2/problema2.java
@@ -19,12 +19,36 @@ public class problema2 {
 
         int[] resultado = new int[2];
 
+        if (izq[0] > drc[0]) {
+            resultado[0] = izq[0];
+
+            if (izq[1] > drc[0]) {
+                resultado[1] = izq[1];
+            } else {
+                resultado[1] = drc[0];
+            }
+
+        } else {
+            resultado[0] = drc[0];
+
+            if (izq[0] > drc[1]) {
+                resultado[1] = izq[0];
+            } else {
+                resultado[1] = drc[1];
+            }
+        }
+
         return resultado;
     }
+
 
     public static void main(String[] args) {
 
         int[] array = {8, 3, 10, 5, 7};
+        int n=4;
+
+        int[] segundomayor = mayores(array, 0, n);
+        System.out.println("segundo mayor: " + segundomayor[1]);
 
     }
 }
